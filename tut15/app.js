a834d8e2c47ffca3f4ac6953087f30ba9c95dc85
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-24 20:01:37 +0530

    Array and array method

---
+++ tut15/app.js
@@ -0,0 +1,49 @@
+
+// ------------------------------- Intro To Arrays
+/* 
+1.  array is reference or non primative data type
+2.  array are mutable so we can change array use array method
+3.  array is ordered collection of items
+4 . we can any data type write in array
+
+*/
+
+// let myArray = ['ravi',45,undefined,true,34.5]
+// console.log(myArray)
+
+// -------
+// console.log(typeof myArray)
+
+// ---- check given object is array
+// console.log(Array.isArray(myArray))
+
+
+// ---------------------- array method
+/*
+1. pop -> remove last element
+2. push ->add  element in last
+3. unshift -> add element in starting
+4. shift ->  remove element in starting
+
+*/
+let myArray = ['ravi',45,undefined,true,34.5]
+console.log(myArray)
+
+// --- pop
+// myArray.pop()
+// console.log(myArray)
+
+// --- push
+// myArray.push('shyam')
+// console.log(myArray)
+
+// --- unshift
+// myArray.unshift('ram')
+// console.log(myArray)
+
+// --- shift
+// myArray.shift()
+// console.log(myArray)
+
+// ----------------------------------- object literal -------------------------------
+// let obj = {}
