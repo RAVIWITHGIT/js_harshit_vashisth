e793d9d8bb2e6a130d4fc851bc74dfcc191ee4be
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-27 19:28:50 +0530

    computed properties in javascript

--- tut21/main.js
+++ tut21/main.js
@@ -3,17 +3,20 @@
 
 // ------------ set object key:value using varaible
 
-const key1 = 'objkey1';
-const key2 = 'objkey2';
+// const key1 = 'objkey1';
+// const key2 = 'objkey2';
 
-const value1 = 'myvalue1';
-const value2 = 'myvalue2';
+// const value1 = 'myvalue1';
+// const value2 = 'myvalue2';
 
-// here set key called computed properties
+//--------------- here set key called computed properties
 
-const obj = {
-    [key1] : value1,
-    [key2] : value2
-}
+// const obj = {
+//     [key1] : value1,
+//     [key2] : value2
+// }
 
-console.log(obj)
+// console.log(obj)
+
+// ----------- here access key using this becasue key is varaible
+// console.log(obj[key1])
