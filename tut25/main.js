fced69c1dc235039a88fc0e9f67c6a31c4f713c3
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-28 21:57:18 +0530

    expression function in javascript

---
+++ tut25/main.js
@@ -0,0 +1,49 @@
+// ----------------------- expression function -----------------------------
+
+// this function called is --> declaring function
+// const wish = function (){
+//     console.log('happy birthday ravi')
+// }
+
+// wish()
+// wish()
+// wish()
+
+
+// --- function with parameter
+// const add = function (a,b){
+//     console.log(a+b)
+// }
+// add(1,2)
+
+
+// if we not pass argument and add parameter then give NaN
+// const myfun = function (a,b){
+//     console.log('a parameter value is',a)
+//     console.log('b parameter value is',b)
+//     console.log(a+b)
+// }
+// myfun()
+
+
+// -----------------
+// const check_odd_even = function (a){
+//     if(a%2==0){
+//         console.log(true)
+//     }
+//     else{
+//         console.log(false)
+//     }
+// }
+
+// check_odd_even(4)
+
+// -- short cut of upper code
+
+// const check_odd_even = function (a){
+//         return a % 2 ==0
+//     }
+    
+// console.log(check_odd_even1(4))
+
+
