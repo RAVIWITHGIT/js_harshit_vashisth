e97cac9ca300acd0685fc19e0e521872343c6805
ravi lakshakar <ravilakshakar336@gmail.com>
2023-11-29 19:55:23 +0530

    defalut parameter and rest operator in function

---
+++ tut30/main.js
@@ -0,0 +1,40 @@
+// ---------------------- defalut parameters --------------------------------------
+
+// ------ without using dealut parameter 
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(4)
+
+// ------- set defalut parameters manually set
+// function add(a,b){
+//     if(typeof b ==='undefined'){
+//         b = 3
+//     }
+//     console.log(a+b)
+// }
+// add(4)
+
+
+// ------ set defalut parameter in function
+// function add(a,b=6){
+//     console.log(a+b)
+// }
+// add(4,1)
+
+// function add(a,b=6){
+//     console.log(a+b)
+// }
+// add(4)
+
+
+
+// --------------------------- spread/rest operator use in function---------------------------------
+
+// function myfun(a,b,...c){
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+// }
+
+// myfun(12,3,4,5,6,7)
