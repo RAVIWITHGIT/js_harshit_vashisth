c368537039fd2968d79dd43c6591bec7c9933611
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-01 19:32:49 +0530

    array method

---
+++ tut33/main.js
@@ -0,0 +1,60 @@
+// -------------------------- array method ---------------------------------------------------------
+
+// -------------------- foreach method
+
+// // before foreach method
+
+// const numbers = [1,2,3,4,5]
+
+// function myfun(number,index){
+//     console.log(`index is ${index} and value is ${number}`)
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     myfun(numbers[i],i)
+    
+// }
+
+
+// **** upper function using foreach loop
+// const numbers = [1,2,3,4,5]
+
+// function myfun(number,index){
+//     console.log(`index is ${index} and value is ${number}`)
+// }
+
+// numbers.forEach(myfun)
+
+// **** more understanding in foreach loop , we access index and value using foreach loop
+
+// const numbers = [1,2,3,4,5]
+
+// numbers.forEach(function(value,index){
+//     console.log(`index is ${index} and value is ${value}`)
+
+
+// })
+
+// const numbers = [1,2,3,4,5]
+
+// numbers.forEach((value,index)=>{
+//     console.log(`index is ${index} and value is ${value}`)
+
+
+// })
+
+
+
+// -------------
+// const users = [
+//     {firstName:'ravi',age:21},
+//     {firstName:'shiv',age:22},
+//     {firstName:'ranu',age:20},
+//     {firstName:'shayam',age:24}
+
+    
+// ]
+
+// users.forEach((obj)=>{
+//     console.log(obj.firstName)
+// })
