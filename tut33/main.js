35c9b38a55582ad3aa85ba526ed585960f890d1d
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-05 19:54:47 +0530

    array method - foreach,map,filter,reduce method

--- tut33/main.js
+++ tut33/main.js
@@ -58,3 +58,134 @@
 // users.forEach((obj)=>{
 //     console.log(obj.firstName)
 // })
+
+
+// ----------------------------------------- map method ------------------------------
+
+// --- map method same work as for each method 
+// ---  for each is not return new array
+
+// let arr = [1,2,3,4,5]
+// let square = function(number){
+//     return number*number
+// }
+
+// let squareNumber = arr.forEach(square)
+// console.log(squareNumber)
+
+// ***************** map method return new array
+
+// let arr = [1,2,3,4,5]
+// let square = function(number){
+//     return number*number
+// }
+
+// let  squareNumber = arr.map(square)
+// console.log(squareNumber)
+
+
+// -----
+// let arr = [1,2,3,4,5]
+
+// let getValue = arr.map((value,index,arr)=>{
+//     return value
+
+// })
+// console.log(getValue)
+
+// ----
+// let arr = [1,2,3,4,5,6,7,8]
+// let getArr = arr.map(function (value,index,arr){
+//     return value
+// })
+// console.log(getArr)
+
+
+// ----
+// let employe_info = [
+//     {
+//         name:'ravi',
+//         age:21,
+//         hobbie:'coding'
+//     },
+//     {
+//         name:'shiv',
+//         age:22,
+//         hobbie:'study'
+//     },
+//     {
+//         name:'ranu',
+//         age:23,
+//         hobbie:'travling'
+//     },
+// ]
+
+// let getObj = employe_info.map((value)=>{
+//     return value.name
+// })
+// console.log(getObj)
+
+
+// ------------------------------------ filter method ------------------------------------------
+
+// -**** filter method same work as map method 
+//  major diffrence is we can not add condition in map method but add condition in filter method, more know about in below
+
+// ---*** use condtion in map method
+
+// const number = [1,2,3,4,5,6]
+
+// let getvalue = number.map((value)=>{
+//     return value%2==0
+
+// })
+// console.log(getvalue)
+
+
+// --------- condtion use in filter method
+
+// const number = [1,2,3,4,5,6]
+
+// let getvalue = number.filter((value)=>{
+//     return value%2==0
+
+// })
+// console.log(getvalue)
+
+
+// --------------------------------------- reduce method --------------------------------------
+
+// --- many use of reduce method , one is below
+// --- if you want sum of all array value then we use reduce method 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// let sumValue = arr.reduce((a,b)=>{
+//     return a+b
+// })
+// console.log(sumValue)
+
+// ---------------
+// let userCart = [
+//     {
+//         productId:1,
+//         productName:'mobile',
+//         price:12000
+//     },
+//     {
+//         productId:2,
+//         productName:'tablet',
+//         price:15000
+//     },
+//     {
+//         productId:3,
+//         productName:'laptop',
+//         price:18000
+//     },
+
+// ]
+
+// let getPrice = userCart.reduce((a,b)=>{
+//     return a+b.price
+// },0)
+// console.log(getPrice)
