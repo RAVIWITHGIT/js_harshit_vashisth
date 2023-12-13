5a857d21a52cfc57da825c6f05e079d366080fe6
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-13 17:54:30 +0530

    set and maps in javascript

--- tut36/main.js
+++ tut36/main.js
@@ -79,8 +79,92 @@ let mySet = new Set([1,2,3,4])
 // console.log(mySet11.size)
 
 
+//-------------------------------------- Maps -------------------------------------------------------
+
+/*
+1. just as the object store key value pair , the map also stores the key values pair
+2. the key of the object may be of any type but it is automatically converted into a string 
+3. we can write key of any data type in map , but key does not convert to string
+4. we can not use for of loop in object but we can use for of loop in map
+
+*/
+
+// ------------******* before starting map repeat of object
+// let myobj = {
+//     name:'ravi',
+//     age:21,
+//     1:true
+// }
+// for (let i in myobj){
+//     console.log(typeof i)
+// }
+
+// -----------------****************** let's start the map
+let myMap = new Map()
+myMap.set('name','ravi')
+myMap.set('age',21)
+myMap.set(1,true)
+myMap.set([1,2,3,4],'myArr')
+myMap.set({'from':'jaipur'},'myObj')
+console.log(myMap)
+
+//------ we can use for of loop in map
+
+// for(let i of myMap){
+//     console.log(i)
+// }
+
+// ------- access key 
+// let getKey = myMap.keys()
+// console.log(getKey)
+// for(let i of myMap.keys()){
+//     console.log(typeof i)
+// }
+
+//-------- access value
+// let getValue = myMap.values()
+// console.log(getValue)
+
+// ------ second method create a map
+// let myMap2 = new Map([
+//     ['name','ravi'],
+//     ['age','21'],
+//     [1,true],
+// ])
+// console.log(myMap2)
 
 
+// ------ access key in map , we can not access value like object , use .get(keyName)  method using acess value  
+// let myMap3 = new Map([
+//     ['name','ravi'],
+//     ['age','21'],
+//     [1,true],
+// ])
 
+
+// console.log(myMap3.get('name'))
+// console.log(myMap3.get(1))
+
+//-----------******* REAL LIFE EXAMPLE
+let person1 = {
+    id:1,
+    firstName:'ravi'
+}
+let person2 = {
+    id:2,
+    firstName:'shiv'
+}
+let newMap = new Map()
+newMap.set(person1,{age:21,from:'jaipur'})
+newMap.set({age:22,from:'bassi'},person2)
+// console.log(newMap)
+// console.log(newMap.get(person1).age)
+
+
+//---------
+// let mykey = newMap.keys()
+// console.log(mykey)
+// console.log(mykey.next().value)
+// console.log(mykey.next().value)
 
 
