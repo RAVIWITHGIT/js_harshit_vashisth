4464937f9a24d2bf03a748acef82fa08448e2aa4
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-26 19:44:46 +0530

    update my code

---
+++ part2/tut1/main.js
@@ -0,0 +1,22 @@
+// ---------------************ See Error in conslole
+
+// ---- here we See Error for diffrent situation
+
+// ----
+
+// console.log(firstName)
+
+// ----
+
+// console.log(firstName)
+// let firstName = 'ravi'
+
+// ----
+// console.log(typeof firstName)
+
+// ----
+// console.log(typeof firstName)
+// let firstName;
+
+
+
